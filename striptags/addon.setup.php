942f3d31b2fb821dd67ddd85a3c611392318cb23
nathanvanbakel <info@voorloper.com>
2019-02-22 13:24:22 +0100

    Initial commit

---
+++ striptags/addon.setup.php
@@ -0,0 +1,11 @@
+<?php
+  
+return array(
+      'author'      => 'Nathan van Bakel',
+      'author_url'  => 'https://voorloper.com',
+      'name'        => 'Strip Tags',
+      'description' => 'Removes HTML tags from a given bit of data.',
+      'version'     => '1.0',
+      'namespace'   => 'Voorloper\striptags',
+      'docs_url'    => 'https://voorloper.com'
+);
---
+++ striptags/pi.striptags.php
@@ -0,0 +1,32 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+	// Removing HTML tags with the PHP strip_tags() function: https://secure.php.net/manual/en/function.strip-tags.php
+	// Example input: <p>Something <strong>with</strong> all <em>sorts</em> of <a href="something.html" title="Testing title">tags</a>.</p>
+	// Example output: Something with all sorts of tags. 
+	// Example usage: {exp:striptags}{your_field_with_tags}{/exp:striptags}
+	
+class striptags {
+	
+	public static $name         = 'Voorloper.com: Strip Tags';
+	public static $version      = '1.0';
+	public static $author       = 'Nathan van Bakel';
+	public static $author_url   = 'https://voorloper.com/';
+	public static $description  = 'Removes tags from a given bit of data';
+	public static $typography   = FALSE;
+	
+	public $return_data = "";
+	
+	public function __construct() {
+	
+	  // Get the information between {exp:striptags} and {/exp:striptags}
+		$string = ee()->TMPL->tagdata;
+		
+		// Strip the tags
+		$return_data = strip_tags($string);
+
+    // Return the cleaned data.
+		$this->return_data = $return_data;
+	}
+
+}
+?>
