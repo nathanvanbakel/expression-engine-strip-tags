100791f734f78b1a37027f73050ddacbbdeaeac0
Nathan van Bakel <info@voorloper.com>
2021-12-09 16:37:55 +0100

    Update addon.setup.php

--- striptags/addon.setup.php
+++ striptags/addon.setup.php
@@ -5,7 +5,7 @@ return array(
       'author_url'  => 'https://voorloper.com',
       'name'        => 'Strip Tags',
       'description' => 'Removes HTML tags from a given bit of data.',
-      'version'     => '1.0',
+      'version'     => '1.0.1',
       'namespace'   => 'Voorloper\striptags',
       'docs_url'    => 'https://github.com/nathanvanbakel/expression-engine-striptags'
 );
